b971243b188bcab811022ad3dca162c9c8906d02
Tate Keller <tatek1993@gmail.com>
2020-03-02 13:37:20 -0600

    Installed yarn dependencies (axios, react, react router dom, redux, thunk), set up PrivateRoute.js

---
+++ potluck-planner/src/utils/privateRoute.js
@@ -0,0 +1,20 @@
+import Reeact from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import { Component } from 'react';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route 
+            {...rest}
+            render={props => 
+                localStorage.getItem('token') ? (
+                    <Component {...props}/>
+                ) : (
+                    <Redirect to = '/login'/>
+                )
+            } 
+        />    
+    )
+}
+
+export default PrivateRoute;
