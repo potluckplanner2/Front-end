e5f87e1007e0de4a62678f7a644a037fe19c1d0f
Tate Keller <tatek1993@gmail.com>
2020-03-05 10:59:54 -0600

    Made one singular mealForm
      *can be used for creating a potluck or editing a potluck
      *put handles in App.js
      *passed handles into mealform using props
      *created isUpdating setIsUpdating boolean in meal.js
      *in meal.js created turnary operator to display the MealCard component if isUpDating is false, and MealForm when isUpdating is true

--- potluck-planner/src/components/dashboard.js
+++ potluck-planner/src/components/dashboard.js
@@ -19,29 +19,6 @@ const Dashboard = () => {
             .catch(err => {
                 console.log('There was an error', err.response);
             })
-
-        // const dummyData = [
-        //     {
-        //         title: "Jim's Birthday",
-        //         date: "April 1, 2020",
-        //         time: '7:00 p.m.',
-        //         location: '123 Main St. New York, NY 12345',
-        //         description: "Jim is turning 30! Let's make his favorite foods!",
-        //         dishes: ['Buffalo Wings', 'Cheeseburgers', 'Apple Pie', 'Beef Tacos', 'Chocolate Cake'],
-        //         guests: ['adam4', 'jenny16', 'bob123', 'tony40', 'rachel36']
-        //     },
-        //     {
-        //         title: "My Graduation",
-        //         date: "May 10, 2020",
-        //         time: '3:00 p.m.',
-        //         location: '123 Main St. New York, NY 12345',
-        //         description: "I am graduating! Let's all make desserts!",
-        //         dishes: ['Blueberrry Muffins', 'Chocolate Chip Cookies', 'Cherry Pie', 'Vanilla Cupcakes', 'Chocolate Cake'],
-        //         guests: ['adam4', 'jenny16', 'bob123', 'tony40', 'rachel36']
-        //         }
-        // ]
-
-        // setPotlucks(dummyData);
     }, [])
 
     return (
