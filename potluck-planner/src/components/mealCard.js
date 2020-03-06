64fcb6d3e57d3a6c8f30d542ea30a0db454b9838
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-03-05 20:30:37 -0500

    styled divs

--- potluck-planner/src/components/mealCard.js
+++ potluck-planner/src/components/mealCard.js
@@ -1,46 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
-import {axiosWithAuth} from '../utils/axiosWithAuth';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import styled from "styled-components";
 
+const MainDiv = styled.div`
+  border: 5px solid #fff5d1;
+  background-color: #ce6c47;
+  width: 50%;
+  margin: 10% auto;
+  border-radius: 5px;
+`;
+
+const APH3 = styled.h3`
+  font-family: "Lobster";
+  font-size: 2.5rem;
+  color: #fff5d1;
+  text-decoration: underline #960200;
+`;
+const APH4 = styled.h4`
+  font-family: "Kalam";
+  font-size: 2rem;
+  color: #ffd046;
+  text-decoration: none;
+`;
+const APH5 = styled.h5`
+  font-family: "Kalam";
+  font-size: 2rem;
+  color: #fff5d1;
+  text-decoration: none;
+`;
+const APP = styled.p`
+  font-family: "Kalam";
+  font-size: 2rem;
+  color: #fff5d1;
+  text-decoration: none;
+`;
 // pair programmed by Aldair Balanzar and Adrian Nasaruk
 
-const MealCard = (props) => {
-    // const [data, setData] = useState([]);
+const MealCard = props => {
+  // const [data, setData] = useState([]);
 
-    // const {id} = useParams();
-    // console.log(id);
+  // const {id} = useParams();
+  // console.log(id);
 
-    // useEffect(() => {
-    //     axiosWithAuth()
-    //     .get(`https://potluck-planner2.herokuapp.com/api/potluck/${id}`)
-    //     .then(res => {
-    //         console.log('res:', res);
-    //         setData(res);
-    //     }) 
-    //     .catch(err => {
-    //         console.log('err:', err);
-    //     })
-    //     console.log(setData());
-    // }, [id])
+  // useEffect(() => {
+  //     axiosWithAuth()
+  //     .get(`https://potluck-planner2.herokuapp.com/api/potluck/${id}`)
+  //     .then(res => {
+  //         console.log('res:', res);
+  //         setData(res);
+  //     })
+  //     .catch(err => {
+  //         console.log('err:', err);
+  //     })
+  //     console.log(setData());
+  // }, [id])
 
-
-    
-        
-    return(
-        <div className='card'>
-            <h3>{props.title}</h3>
-            <h5>{props.date} at </h5>
-            <h4>{props.location}</h4>
-            <div className='card-description'>
-                <p>{props.description}</p>
-            </div>
-        
-        </div>
-    )
-        
-    
-}
+  return (
+    <MainDiv className="card">
+      <APH3>{props.title}</APH3>
+      <APH5>{props.date} at </APH5>
+      <APH4>{props.location}</APH4>
+      <div className="card-description">
+        <APP>{props.description}</APP>
+      </div>
+    </MainDiv>
+  );
+};
 
 export default MealCard;
